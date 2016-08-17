0c07a562ea6112a14119794320c433d9c877e60a
samiakarima <samia.karima@hotmail.com>
2016-08-17 17:57:29 -0300

    Update batalhanaval.cpp

--- batalhanaval.cpp
+++ batalhanaval.cpp
@@ -1,20 +1,25 @@
 //Temas 6, Batalha Naval .-.
 #include <iostream>
 using namespace std;
+
 #define LINHAS 9
 #define COLUNAS 9 
+
 /*
 https://murilo.wordpress.com/2009/05/29/o-que-sao-enums-e-como-utiliza-los-melhor-em-c/
 */
+
 enum Estado{
 	AGUA, 
 	BARCO,
 	ERRO,
 	ACERTO
 };
-
-
+//Funções.
+void mostrar_tabuleiro(int tabuleiro[LINHAS][COLUNAS], int tamanho);
+void adicionar_barcos(int tabuleiro[LINHAS][COLUNAS], int tamanho);
 int main(){
+		int tamanho=5;
 		//Criar o tabuleiro.
 		int tabuleiro[LINHAS][COLUNAS];
 	
@@ -22,30 +27,77 @@ int main(){
 		for(int lin = 0; lin < LINHAS; lin++){
 			for(int col = 0; col < COLUNAS; col++){
 			//0: AGUA. 1:BARCO.
-			tabuleiro[lin][col]=AGUA;	
+			tabuleiro[lin][col]=AGUA;
 			}		
 		}
-		//Marcação de cima
-		cout << "    1  2  3  4  5  6  7  8  9 " <<endl;
-		cout << endl;
+	
 		//Adicionar os barcos.
-		tabuleiro[4][3] = BARCO;
-		tabuleiro[8][5] = BARCO;
-		tabuleiro[5][8] = BARCO;
-		
+		adicionar_barcos(tabuleiro, tamanho);
+			
+	
 		//Mostrar o tabuleiro em tela.
-		for(int lin = 0; lin < LINHAS; lin++){
+		mostrar_tabuleiro(tabuleiro, tamanho);
+
+
+
+
+} 
+
+void mostrar_tabuleiro(int tabuleiro[LINHAS][COLUNAS], int tamanho){
+	cout << "    1  2  3  4  5  6  7  8  9 " <<endl;
+		cout << endl;
+	for(int lin = 0; lin < LINHAS; lin++){
 			cout << " " << lin+1 << " ";
 			for(int col = 0; col < COLUNAS; col++){
-			if(tabuleiro[lin][col]==AGUA){
-				cout << " ~ ";
-			} else if(tabuleiro[lin][col]==BARCO) {
-				cout << " B ";
+				switch(tabuleiro[lin][col])
+				{
+					case AGUA:
+						cout << " ~ ";	
+						break;	
+					case BARCO:
+						cout << " B ";	
+						break;	
+					case ERRO:
+						cout << " X ";	
+						break;	
+					case ACERTO:
+						cout << " O ";
+						break;	
 			}
-				
+	
 			}
 			cout << endl;	
 		
 		}
 		
+	
+}
+void adicionar_barcos(int tabuleiro[LINHAS][COLUNAS], int tamanho){
+		int pos_linha, pos_coluna;
+		cout << "Adicionando Barcos: "<< endl;
+		cout << "Digite a linha [1 ate 9]: " << endl;
+		cin  >> pos_linha;
+		cout << "Digite a coluna [1 ate 9]: " << endl;
+		cin  >> pos_coluna;
+		
+		
+		if(	pos_linha>=1 &&
+			pos_linha<=9 &&
+			pos_coluna>=1 &&
+			pos_coluna<=9 &&
+			pos_coluna+tamanho<=9){
+				
+		
+				for(int aumento = 0 ; aumento < tamanho ; aumento++){
+					
+					tabuleiro[pos_linha-1][pos_coluna-1+aumento] = BARCO;
+		
+				} 
+
+				cout << "Adicionado com sucesso!" << endl;
+		} else {
+			cout << "ERRO! POSICAO INVALIDA!" << endl;
+		}
+			
+			
 }
